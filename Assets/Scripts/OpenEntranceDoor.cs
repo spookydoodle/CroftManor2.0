0a8a53215ec1435f9c32219b73f32d1a64e6532c
kxkaro <kar.krajewska@gmail.com>
2019-03-17 14:04:44 +0100

    Door Animations with colliders working

---
+++ Assets/Scripts/OpenEntranceDoor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenEntranceDoor : MonoBehaviour
+{
+    public Animator buttonAnim;
+    public GameObject door;
+
+    private bool isPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator buttonAnim = gameObject.GetComponent<Animator>();
+        GameObject door = gameObject.GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool key = Input.GetKeyDown(KeyCode.E);
+
+        Animating(key);
+        Debug.Log(isPressed);
+    }
+
+    // Set parameters used in conditions of transitions in Animator component
+    void Animating(bool key)
+    {
+
+        if (key && !isPressed)
+        {
+            buttonAnim.SetTrigger("Press");
+            
+            door.transform.Rotate(0, -90, 0, Space.Self);
+
+            isPressed = true;
+        }
+        else if (key && isPressed)
+        {
+            buttonAnim.SetTrigger("Release");
+
+            door.transform.Rotate(0, 90, 0, Space.Self);
+
+            isPressed = false;
+        }
+    }
+}
