edbc8550c0f2144418b483870dc8e635a2524588
rwakulszowa <rwakulszowa1@gmail.com>
2019-03-25 23:50:37 +0100

    handle midair jumps

--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PlayerCharacterController : MonoBehaviour {
 
-    public float speed = 1.5f;
+    // Constants
+    public float moveSpeed = 5.0f;
     public float rotationSpeed = 75.0f;
-    public float jumpSpeed = 50.0f;
-    public float gravity = 10.0f;
-    float ySpeed = 0;
-    private bool isGrounded = false;
+    public float jumpSpeed = 25.0f;
+    public float gravity = 5.0f;
+
+    // State
     private bool isSwimming = false;
 
+    // MovementState
+    Vector3 speed = Vector3.zero;
+
+    // Unity
     CharacterController _controller;
     public Animator anim;
-    Vector3 moveDirection = Vector3.zero;
 
     // Use this for initialization
     void Start()
@@ -23,22 +27,14 @@ public class PlayerCharacterController : MonoBehaviour {
         Animator anim = gameObject.GetComponent<Animator>();
     }
 
-
     // Detect collision with floor
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.transform.tag == "Floor") {
-            isGrounded = true;
-            ySpeed = 0;
-        }
     }
-    //void OnCollisionExit(Collision collision)
-    //{
-    //    if (collision.gameObject.transform.tag == "Floor")
-    //        isGrounded = false;
-    //}
 
-
+    void OnCollisionExit(Collision collision)
+    {
+    }
 
     // Update is called once per frame
     void Update() {
@@ -47,20 +43,14 @@ public class PlayerCharacterController : MonoBehaviour {
         float f = Input.GetAxisRaw("Fire1");
         bool j = Input.GetButtonDown("Jump");
 
-        // Jump if key is pressed
         if (j)
         {
-            Jump(j, jumpSpeed);
+            Jump();
         }
 
-        // Fall downwards
         Fall();
-
         
-        // Move forwards/backwards
         MoveForward(v);
-        
-       
 
         // Reverse rotation when moving backwards
         if (h != 0f && v < 0f)
@@ -78,70 +68,45 @@ public class PlayerCharacterController : MonoBehaviour {
             Rotate(h);
         }
 
-        // Check if player is grounded AFTER movement
-        setGravity();
-        
+        Move();
 
         // Set animations for movement
         Animating(v, h);
-
     }
-
-
-    void setGravity()
-    {
-        if (_controller.isGrounded) {
-            ySpeed = 0;
-        }
-    }
-
 
     void MoveForward(float v)
     {
-        //if (_controller.isGrounded)
-        //{
-            moveDirection = new Vector3(0, 0, v);
-            moveDirection = speed * transform.TransformDirection(moveDirection);
-            transform.Translate(0, 0, speed * v * Time.deltaTime);
-        //}
-
-        _controller.Move(moveDirection * Time.deltaTime);
+        speed.z = moveSpeed * v;
     }
-
 
     void MoveSideways(float h)
     {
-        //if (_controller.isGrounded)
-        //{
-            moveDirection = new Vector3(h, 0, 0);
-            moveDirection = speed * transform.TransformDirection(moveDirection);
-            transform.Translate(speed * h * Time.deltaTime, 0, 0);
-        //}
-
-        _controller.Move(moveDirection * Time.deltaTime);
-
+        speed.x = h;
     }
-
 
     void Rotate(float h)
     {
         transform.Rotate(0.0f, rotationSpeed * h * Time.deltaTime, 0.0f);
     }
 
-
-    void Jump(bool j, float jumpSpeed)
+    void Jump()
     {
-        ySpeed = jumpSpeed;
+        if (_controller.isGrounded) {
+            speed.y = jumpSpeed;
+        }
     }
-
 
     void Fall()
     {
-        ySpeed -= gravity;
-        moveDirection = new Vector3(0, ySpeed, 0);
-        _controller.Move(moveDirection * Time.deltaTime);
+        if (!_controller.isGrounded) {
+          speed.y -= gravity;
+        }
     }
 
+    void Move()
+    {
+        _controller.Move(transform.TransformDirection((speed * Time.deltaTime)));
+    }
 
     // Set parameters used in conditions of transitions in Animator component
     void Animating(float v, float h)
@@ -149,5 +114,4 @@ public class PlayerCharacterController : MonoBehaviour {
         bool walking = h != 0f || v != 0f;
         anim.SetBool("IsWalking", walking);
     }
-
 }
