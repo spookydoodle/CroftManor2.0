1db51f3772558d31cfef99957851caa91b2bb61a
rwakulszowa <rwakulszowa1@gmail.com>
2019-04-06 15:56:41 +0200

    Simplify Camera logic

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,57 +4,26 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    public GameObject target;            // The position that that camera will be following.
-    Vector3 offset;                     // The initial offset from the target.\
-    float mouseX, mouseY;
-    public float RotationSpeed = 1.0f;
+    public GameObject target;  // The position that that camera will be following.
+
+    private Vector3 offset;  // The initial offset from the target.
 
     void Start()
     {
-        // Calculate the initial offset.
-        offset = target.transform.position - transform.position;
-        //Cursor.visible = false;
-        //Cursor.lockState = CursorLockMode.Locked;
+       offset = target.transform.position - transform.position;
     }
 
-    //void FixedUpdate()
-    //{
-    //    // Create a postion the camera is aiming for based on the offset from the target.
-    //    Vector3 targetCamPos = target.position + offset;
-
-    //    // Smoothly interpolate between the camera's current position and it's target position.
-    //    transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-    //}
-
-    void LateUpdate()
+    void Update()
     {
-        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
-        float angle = target.transform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, angle+mouseX, 0);
-        Quaternion camRotation = Quaternion.Euler(mouseY, mouseX, 0);
-
-        transform.LookAt(target.transform);
-
-
-        transform.position = target.transform.position - (rotation * offset);
-        transform.rotation = target.transform.rotation * camRotation;
-        //transform.position = target.transform.position - offset;
-        //target.transform.rotation = Quaternion.Euler(0, mouseX, 0);
-
-        //CamControl();
+       SetCameraTransform();
     }
 
-
-    void CamControl()
+    void SetCameraTransform()
     {
+        // Copy the target's transform
+        transform.SetPositionAndRotation(target.transform.position, target.transform.rotation);
         
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
-
-        transform.LookAt(target.transform);
-
-        target.transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-        //player.transform.rotation = Quaternion.Euler(0, mouseX, 0);
-
+        // Translate by the offset *after* setting the rotation -> translation takes rotation into account
+        transform.Translate(-offset);
     }
 }
