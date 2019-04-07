ad2c40930b2732838dcfccdb0cd4c17051268232
kxkaro <kar.krajewska@gmail.com>
2019-04-07 18:29:10 +0200

    Added invert y axis to camfollow script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
     
     public GameObject target;  // The position that that camera will be following.
-    
+    public bool invertYaxis = false;
+
     private float rotationSpeed = 1f;
     private Vector3 offset;  // The initial offset from the target.
     private Vector3 rotation = Vector3.zero;
+    private int setInvYax;
 
     void Start()
     {
         offset = target.transform.position - transform.position;
+        handleSettings(invertYaxis);
     }
 
     void Update()
     {
         float y = Input.GetAxis("Mouse Y");
-        HandleRotation(y * Settings.MouseSensitivity());
+        HandleRotation(setInvYax * y * Settings.MouseSensitivity());
         SetCameraTransform();
     }
 
@@ -27,7 +30,8 @@ public class CameraFollow : MonoBehaviour {
         rotation.x += rotationValue * rotationSpeed;
 
         // Limit the rotation to <-45: Down, 90: Up>
-        rotation.x = Mathf.Clamp(rotation.x, -90f, 90f);
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 50f);
+        Debug.Log(rotation.x);
     }
 
     void SetCameraTransform()
@@ -39,5 +43,13 @@ public class CameraFollow : MonoBehaviour {
         
         // Translate by the offset *after* setting the rotation -> translation takes rotation into account
         transform.Translate(-offset);
+    }
+
+    void handleSettings(bool invertYaxis)
+    {
+        if (invertYaxis)
+            setInvYax = -1;
+        else
+            setInvYax = 1;
     }
 }
