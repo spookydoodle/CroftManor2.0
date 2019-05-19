fcf8a476db2e1bcd980533f1abedec8eba87ac70
rwakulszowa <rwakulszowa1@gmail.com>
2019-05-19 21:19:08 +0200

    Bullet controller

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private float speed = 10;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+        this.rb.velocity = this.transform.forward * speed;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        // Destroy the bullet on any collision
+        Destroy(this.gameObject);
+    }
+}
