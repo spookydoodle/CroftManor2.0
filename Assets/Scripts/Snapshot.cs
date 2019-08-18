bec1de357143ec2c97f9a54779f2a4fbfc70b626
kxkaro <kar.krajewska@gmail.com>
2019-08-18 23:28:37 +0200

    Snapshot camera render

---
+++ Assets/Scripts/Snapshot.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+
+public class Snapshot : MonoBehaviour
+{
+    Camera snapCam;
+
+    int resWidth = 256;
+    int resHeight = 256;
+
+    void Awake()
+    {
+        snapCam = GetComponent<Camera>();
+        if(snapCam.targetTexture == null)
+        {
+            snapCam.targetTexture = new RenderTexture(resWidth, resHeight, 24);
+        }
+        else
+        {
+            resWidth = snapCam.targetTexture.width;
+            resHeight = snapCam.targetTexture.height;
+        }
+
+        snapCam.gameObject.SetActive(false);
+    }
+
+
+    public void CallTakeSnapshot()
+    {
+        snapCam.gameObject.SetActive(true);
+    }
+
+    void LateUpdate()
+    {
+        if (snapCam.gameObject.activeInHierarchy)
+        {
+            Texture2D snapshot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            snapCam.Render();
+            RenderTexture.active = snapCam.targetTexture;
+            snapshot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            byte[] bytes = snapshot.EncodeToPNG();
+            string filename = SnapshotName();
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log("snapshot taken" + filename);
+            snapCam.gameObject.SetActive(false);
+        }
+
+        string SnapshotName()
+        {
+            return string.Format("{0}/Snapshots/snap_{1}x{2}_{3}.png",
+            Application.dataPath,
+            resWidth,
+            resHeight,
+            System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+      
+        }
+    }
+}
---
+++ Assets/Scripts/SnapshotCamRef.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapshotCamRef : MonoBehaviour
+{
+    public Snapshot snapCam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            snapCam.CallTakeSnapshot();
+        }
+    }
+}
