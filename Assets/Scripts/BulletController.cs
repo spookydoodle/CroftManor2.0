5d68c585cf243c836cdb528ee3ad6b1aafbc42cc
rwakulszowa <rwakulszowa1@gmail.com>
2019-09-10 09:26:56 +0200

    BaseProjectileController on top of BulletController

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -2,31 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletController : MonoBehaviour
+public class BulletController : BaseProjectileController
 {
-    private static float speed = 10;
-    private Rigidbody rb;
-
-    void Start()
-    {
-        this.rb = GetComponent<Rigidbody>();
-        this.rb.velocity = this.transform.forward * speed;
-        this.rb.useGravity = this.UsesGravity();
-    }
-
-    public Vector3 InitialSpeed()
-    {
-        return new Vector3(0, 0, speed);
-    }
-
-    public bool UsesGravity()
-    {
-        return false;
-    }
-
-    void OnCollisionEnter(Collision other)
-    {
-        // Destroy the bullet on any collision
-        Destroy(this.gameObject);
-    }
+  public override Vector3 InitialSpeed() { return new Vector3(0, 0, 20); }
+  public override bool UsesGravity() { return false; }
 }
